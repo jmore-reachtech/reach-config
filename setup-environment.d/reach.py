c20059c6aa5bd872d67e5f9f7a39e0dd0ab88117
Fabio Berton <fabio.berton@ossystems.com.br>
2014-08-07 09:21:15 -0300

    Initial configuration structure for Reach Rocko
    Signed-off-by: Fabio Berton <fabio.berton@ossystems.com.br>

---
+++ setup-environment.d/reach.py
@@ -0,0 +1,17 @@
+def __set_defaults_reach_yocto():
+    set_default('DISTRO', 'reach')
+
+def __after_init_reach_yocto():
+    PLATFORM_ROOT_DIR = os.environ['PLATFORM_ROOT_DIR']
+    append_layers([ os.path.join(PLATFORM_ROOT_DIR, 'sources', p) for p in
+                    [
+                        'meta-freescale',
+                        'meta-openembedded/meta-oe',
+                        'meta-reach',
+                    ]])
+
+    # FSL EULA
+    eulas.accept['meta-freescale/EULA'] = 'ACCEPT_FSL_EULA = "1"'
+
+run_set_defaults(__set_defaults_reach_yocto)
+run_after_init(__after_init_reach_yocto)
