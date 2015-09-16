c8f1e25e24db6b462b1d4df19a61d0ab2888344a
Mario Domenech Goulart <mario@ossystems.com.br>
2015-09-16 16:45:14 -0300

    Don't complain about missing DISTRO or MACHINE if local.conf exists
    If local.conf exists, assume it contains correct values for
    DISTRO and MACHINE, if they have not been provided in the
    environment.  If DISTRO and/or MACHINE have been provided in the
    environment and are valid, use the provided ones.
    
    If local.conf doesn't exist, require valid values for both DISTRO
    and MACHINE in the environment.
    
    Signed-off-by: Mario Domenech Goulart <mario@ossystems.com.br>

--- setup-environment.d/reach.py
+++ setup-environment.d/reach.py
@@ -8,28 +8,33 @@ def __set_defaults_reach_yocto():
                       'g2c-mfg', 'g2h-solo-1', 'g2h-solo-2',
                       'g2h-solo-3', 'g2h-solo-4', 'g2s-101-24']
 
-    try:
-        distro = os.environ['DISTRO']
-    except KeyError:
-        distro = None
+    local_conf_exists = os.path.isfile(os.path.join(build_dir,
+                                                    'conf',
+                                                    'local.conf'))
 
-    try:
-        machine = os.environ['MACHINE']
-    except KeyError:
-        machine = None
-
-    if not distro or distro not in valid_distros:
-        sys.stderr.write("ERROR: You must set 'DISTRO' before setting up the environment.\n")
-        sys.stderr.write("       Possible values are %s\n" % valid_distros)
+    def required_var_error(varname, valid_vals):
+        sys.stderr.write("ERROR: You must set '%s' before setting up the environment.\n" %
+                         (varname,))
+        sys.stderr.write("       Possible values are %s\n" % valid_vals)
         sys.exit(1)
 
-    if not machine or machine not in valid_machines:
-        sys.stderr.write("ERROR: You must set 'MACHINE' before setting up the environment.\n")
-        sys.stderr.write("       Possible values are %s\n" % valid_machines)
-        sys.exit(1)
+    def maybe_set_default(varname, valid_vals):
+        try:
+            val = os.environ[varname]
+        except KeyError:
+            val = None
 
-    set_default('DISTRO', os.environ['DISTRO'])
-    set_default('MACHINE', os.environ['MACHINE'])
+        if val:
+            if val in valid_vals:
+                set_default(varname, val)
+            else:
+                required_var_error(varname, valid_vals)
+        elif not local_conf_exists:
+            required_var_error(varname, valid_vals)
+
+    maybe_set_default('DISTRO', valid_distros)
+    maybe_set_default('MACHINE', valid_machines)
+
 
 def __after_init_reach_yocto():
     PLATFORM_ROOT_DIR = os.environ['PLATFORM_ROOT_DIR']
